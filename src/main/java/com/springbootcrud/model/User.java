ea0faf3e6264adc63c4605d16f664f454ef75b23
Verda05 <verda1988@gmail.com>
2022-04-12 19:49:36 +0300

    Амирханов Б.А.

---
+++ src/main/java/com/springbootcrud/model/User.java
@@ -0,0 +1,65 @@
+package com.springbootcrud.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    //Почта должна быть уникальной
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 15, nullable = false)
+    private String password;
+
+    @Column(length = 45,nullable = false, name = "name")
+    private String name;
+
+    @Column(length = 45,nullable = false, name = "lastname")
+    private String lastname;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
